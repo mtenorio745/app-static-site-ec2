db0d7b6e622679c68f2ea30a9586dc9d19f8f55b
mateus tenorio <mateus.tenorio@hotmail.com>
2023-08-30 22:36:40 -0300

    tentativa15

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 module "network" {
-  source  = "./network"
+  source  = "./modules/network"
   vpc_cidr = "10.0.0.0/16"
 }
 
 module "compute" {
-  source  = "./compute"
+  source  = "./modules/compute"
   vpc_id  = module.network.vpc_id
   subnet_id = module.network.subnet_id
   sg_id = module.network.sg_id
