6f44ce44227dd137743f31324b4f7744544fb958
mateus tenorio <mateus.tenorio@hotmail.com>
2023-08-30 22:28:19 -0300

    tentativa13

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ module "compute" {
     ec2_lt_ami           = var.ec2_lt_ami
     ec2_lt_instance_type = var.ec2_lt_instance_type
     ec2_lt_ssh_key_name  = var.ec2_lt_ssh_key_name
+    
     vpc_id               = module.network.vpc_id
     vpc_sn_pub_az1_id    = module.network.vpc_sn_pub_az1_id
     vpc_sg_pub_id        = module.network.vpc_sg_pub_id
