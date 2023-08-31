a713276e6324c4cef34ac3d88e3a0f104a930eb6
mateus tenorio <mateus.tenorio@hotmail.com>
2023-08-30 22:40:31 -0300

    tentativa16

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "example" {
-  ami           = "ami-02e136e904f3da870" # Make sure to use the correct AMI
+  ami           = "ami-02e136e904f3da870" 
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
   key_name = "your_key_name_here"
 
   tags = {
-    Name = "example-instance"
+    Name = "mateus"
   }
 }
