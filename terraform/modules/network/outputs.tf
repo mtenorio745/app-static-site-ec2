4b89a96377065643aaa936e6073cad4c602cdcf3
mateus tenorio <mateus.tenorio@hotmail.com>
2023-08-30 21:58:48 -0300

    tentativa8

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,21 +1,27 @@
-# NETWORK OUTPUT TO BE REUSED
-
 output "vpc_id" {
-    value = "${aws_vpc.vpc.id}"
+  value = aws_vpc.vpc.id
 }
 
-output "vpc_sg_pub_id" {
-    value = "${aws_security_group.vpc_sg_pub.id}"
+output "vpc_cidr" {
+  value = aws_vpc.vpc.cidr_block
 }
 
-output "vpc_sg_priv_id" {
-    value = "${aws_security_group.vpc_sg_priv.id}"
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
 }
 
-output "vpc_sn_pub_az1_id" {
-    value = "${aws_subnet.sn_pub_az1.id}"
+output "subnet_public_az1_id" {
+  value = aws_subnet.sn_pub_az1.id
 }
 
-output "vpc_sn_priv_az1_id" {
-    value = "${aws_subnet.sn_priv_az1.id}"
+output "route_table_public_id" {
+  value = aws_route_table.rt_pub.id
+}
+
+output "security_group_public_id" {
+  value = aws_security_group.vpc_sg_pub.id
+}
+
+output "availability_zone" {
+  value = var.vpc_az1
 }
